855b5a54beb9eb99048e3803c336afbf236a5819
Denis Wolter <wolter.important@gmail.com>
2023-06-20 21:21:24 +0300

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './footer.scss'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <p>e</p>
+      <p className='mail-text'>ewwewe.wewew@gmail.com</p>
+      <div className="text-container">
+        <p className="text">Компания "Ischenko"</p>
+        <p className="text">Контакты +375337777777</p>
+      </div>
+      <div>
+        <a href='https://www.instagram.com' className='link'>
+          <InstagramIcon sx={{ fontSize: 44, color: 'white' }}/>
+        </a>
+        <a href='https://youtube.com' className='link'>
+          <YouTubeIcon sx={{ fontSize: 44, color: 'white' }}/>
+        </a>
+      </div>
+
     </div>
   );
 };
