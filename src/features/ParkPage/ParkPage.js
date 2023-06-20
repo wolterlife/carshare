3ede8b802ffc36e76253a1824ec617394c02ce9a
Denis Wolter <wolter.important@gmail.com>
2023-06-20 23:51:16 +0300

    add price page

--- src/features/ParkPage/ParkPage.js
+++ src/features/ParkPage/ParkPage.js
@@ -9,35 +9,35 @@ const ParkPage = () => {
       <Header/>
       <div className="parkList">
         <div className='card'>
-          <p className='card-text'>VOLKSWAGEN POLO</p>
+          <p className='card-text'>VOLKSWAGEN POLO (эконом)</p>
           <img className='image-car' src='/img/vw.png' alt='vw'/>
         </div>
         <div className='card'>
-          <p className='card-text'>RENAULT LOGAN</p>
+          <p className='card-text'>RENAULT LOGAN (эконом)</p>
           <img className='image-car' src='/img/reno.png' alt='reno'/>
         </div>
         <div className='card'>
-          <p className='card-text'>Toyota Camry</p>
+          <p className='card-text'>Toyota Camry (эконом)</p>
           <img className='image-car' src='/img/ty_camri.png' alt='ty_camri'/>
         </div>
         <div className='card'>
-          <p className='card-text'>LADA VESTA CROSS</p>
+          <p className='card-text'>LADA VESTA CROSS (эконом)</p>
           <img className='image-car' src='/img/lada-vesta.png' alt='solaris'/>
         </div>
         <div className='card'>
-          <p className='card-text'>KIA OPTIMA</p>
+          <p className='card-text'>KIA OPTIMA (бизнес)</p>
           <img className='image-car' src='/img/kia-opt.png' alt='solaris'/>
         </div>
         <div className='card'>
-          <p className='card-text'>HONDA ACCORD</p>
+          <p className='card-text'>HONDA ACCORD (бизнес)</p>
           <img className='image-car' src='/img/hend-acc.png' alt='solaris'/>
         </div>
         <div className='card'>
-          <p className='card-text'>LIFAN SOLANO II</p>
+          <p className='card-text'>LIFAN SOLANO II (премиум)</p>
           <img className='image-car' src='/img/lifan.png' alt='solaris'/>
         </div>
         <div className='card'>
-          <p className='card-text'>NISSAN ALMERA</p>
+          <p className='card-text'>NISSAN ALMERA (премиум)</p>
           <img className='image-car' src='/img/almera.png' alt='solaris'/>
         </div>
       </div>
--- src/features/PricePage/PricePage.js
+++ src/features/PricePage/PricePage.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import './pricePage.scss'
 
 const PricePage = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+        <p className='text-price'>Тарифы</p>
+      <div className='price-cards'>
+        <div className='card firstCard'>
+          <p className='card-text-top'>Эконом</p>
+          <img className='image-car' src='/img/vw.png' alt='vw'/>
+          <div className='card-info-container'>
+            <p className='card-text-item'>1 день - 53 BYN</p>
+            <p className='card-text-item'>1 час - 15 BYN</p>
+            <p className='card-text-item'>1 минута - 0.17 BYN</p>
+          </div>
+        </div>
+        <div className='card secondCard'>
+          <p className='card-text-top'>Бизнес</p>
+          <img className='image-car' src='/img/hend-acc.png' alt='solaris'/>
+          <div className='card-info-container'>
+            <p className='card-text-item text-black'>1 день - 67 BYN</p>
+            <p className='card-text-item text-black'>1 час - 23 BYN</p>
+            <p className='card-text-item text-black'>1 минута - 0.27 BYN</p>
+          </div>
+        </div>
+        <div className='card lastCard'>
+          <p className='card-text-top'>Премиум</p>
+          <img className='image-car' src='/img/almera.png' alt='solaris'/>
+          <div className='card-info-container'>
+            <p className='card-text-item'>1 день - 72 BYN</p>
+            <p className='card-text-item'>1 час - 27 BYN</p>
+            <p className='card-text-item'>1 минута - 0.36</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
